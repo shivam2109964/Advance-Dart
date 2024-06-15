107d14b26b7d4f6960b482ce7dc426c9e9e3347e
shivam_since2002 <sg2109964@gmail.com>
2024-06-15 18:43:56 +0530

    15 June

--- Built-In Types/String/StringCodeUnit.dart
+++ Built-In Types/String/StringCodeUnit.dart
@@ -16,6 +16,24 @@ void main() {
   print(que1D);
   var que1M = manuallyMethRevStr(manuallyCodeUnit);
   print(que1M);
+
+  //Que2 Result direct and manually
+  var que2D = stringToHexaDirect(name);
+  print(que2D);
+  var que2M = stringToHexaManually(name);
+  print(que2M);
+
+  //Que3 Result direct
+  var que3D = findRangeDirect(name);
+  print(que3D);
+
+  var que3Test = findRangeDirect("hsdlHSUkskI");
+  print(que3Test);
+  print("Totol Characters find is ${que3Test.length}");
+
+  //Que4 Result direct and manually
+  var que4D = sumOfAllUniCodeDirect(name);
+  print(que4D);
 }
 
 List<int> generateCodeUnit(String str) {
@@ -53,4 +71,76 @@ String? manuallyMethRevStr(List<int> codeUnits) {
     }
   }
   return data.join();
+}
+
+///Que2. String to Hexadecimal Conversion
+///Direct
+List stringToHexaDirect(String str) {
+  List<int> convertToCodeUnit = [];
+  List convertToHexa = [];
+  for (int i = 0; i <= str.length - 1; i++) {
+    convertToCodeUnit.add(str.codeUnitAt(i));
+  }
+
+  for (int element in convertToCodeUnit) {
+    var data = element.toRadixString(16);
+    convertToHexa.add(data);
+  }
+  return convertToHexa;
+}
+
+/// Manually
+List stringToHexaManually(String str) {
+  List<int> convertToCodeUnit = [];
+  List<String> hexValues = [];
+
+  for (int i = 0; i <= str.length - 1; i++) {
+    convertToCodeUnit.add(str.codeUnitAt(i));
+  }
+  for (int element in convertToCodeUnit) {
+    hexValues.add(hexaOperation(element));
+  }
+  return hexValues;
+}
+
+String hexaOperation(int singleData) {
+  const hexChars = "0123456789ABCDEF";
+  String hexResult = "";
+
+  do {
+    int remainder = singleData % 16;
+    hexResult = hexChars[remainder] + hexResult;
+    singleData = singleData ~/ 16;
+  } while (singleData > 0);
+  return hexResult;
+}
+
+/// Que3. Count Characters with Specific Code Unit Range
+/// Write a function that counts how many characters in a string have Unicode code units within
+/// a specified range (e.g., betweeen 65 and 90 for Upper case letters )
+
+List findRangeDirect(String str) {
+  List<int> convertIntoCodeUnit = [];
+  List findCharInRange = [];
+  for (int i = 0; i <= str.length - 1; i++) {
+    convertIntoCodeUnit.add(str.codeUnitAt(i));
+  }
+  for (int i = 0; i <= convertIntoCodeUnit.length - 1; i++) {
+    if (convertIntoCodeUnit[i] >= 65 && convertIntoCodeUnit[i] <= 90) {
+      findCharInRange.add(String.fromCharCode(convertIntoCodeUnit[i]));
+    }
+  }
+  return findCharInRange;
+}
+
+/// Unicode Code Unit Sum
+/// Develop a function that computers the sum of all Unicode units in a given string.
+int sumOfAllUniCodeDirect(String str) {
+  List<int> convertToUniCode = [];
+  for (int i = 0; i <= str.length - 1; i++) {
+    convertToUniCode.add(str.codeUnitAt(i));
+  }
+  return convertToUniCode.reduce((value, element) {
+    return value + element;
+  });
 }
